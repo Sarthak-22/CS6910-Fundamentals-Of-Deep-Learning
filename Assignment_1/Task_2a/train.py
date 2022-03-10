999d1433a5d8c5d1f3fb0c28ee08acee3d0c107a
Akshat Joshi <akshatjoshi375@gmail.com>
2022-03-10 14:09:04 +0530

    Updated for pattern mode

--- Assignment_1/Task_2a/train.py
+++ Assignment_1/Task_2a/train.py
@@ -6,17 +6,21 @@ import torch.nn  as nn
 
 criterion = nn.BCELoss()
 
-
-batch_size = 10
-epochs = 200
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+batch_size = 1
+epochs = 300
 
 trainDataset = Task2aDataset("dataset/train_t25.csv")
 trainLoader  = torch.utils.data.DataLoader(trainDataset, batch_size=batch_size,
                                           shuffle=True)
 model = FFNN()
-optimizer = optim.SGD(model.parameters(),lr=0.001,momentum=0.9)
+model.to(device)
+
+optimizer = optim.SGD(model.parameters(),lr=0.0001,momentum=0.7)
 
 
+
+print(device)
 
 for epoch in range(epochs):
     print("Current Epoch: ",epoch)
@@ -26,10 +30,11 @@ for epoch in range(epochs):
         cnt+=1
         optimizer.zero_grad()
         inputs, labels = data
-
+        inputs = inputs.to(device)
+        labels = labels.to(device)
         # Model
-        hl1outputs = model(inputs,path='hidden1')
-        hl2outputs = model(hl1outputs,path='hidden2')
+        hl1outputs = model(inputs,path='hidden1').to(device)
+        hl2outputs = model(hl1outputs,path='hidden2').to(device)
         outputs = model(hl2outputs,path='output')
 
 
